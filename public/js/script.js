58dfe66673dbfa23a2df8e84690f3f461168a1fb
Abel Mendez <amsiix@gmail.com>
2023-04-27 17:34:39 +0000

    changed index and script.js files

--- public/js/script.js
+++ public/js/script.js
@@ -1,40 +1,45 @@
-function addMessageToChatHistory(message) {
-    const chatHistory = document.getElementById("chat-history");
-    const messageElement = document.createElement("div");
-    messageElement.textContent = message;
-    chatHistory.appendChild(messageElement);
-}
-
-
-const API_URL = "http://localhost:3000/api";
-
-// Fetch existing messages when the page loads
-window.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch(`${API_URL}/messages`);
-    const messages = await response.json();
-
-    for (const message of messages) {
-        addMessageToChatHistory(message.content);
-    }
-});
-
-document.getElementById("chat-form").addEventListener("submit", async function (event) {
-    event.preventDefault();
-
-    const messageInput = document.getElementById("message");
-    const messageText = messageInput.value;
-
-    // Add message to the backend
-    await fetch(`${API_URL}/messages`, {
-        method: "POST",
-        headers: {
+document.addEventListener("DOMContentLoaded", () => {
+    const API_URL = "http://localhost:3000/api";
+    const submitBtn = document.getElementById("submit-btn");
+    const messageInput = document.getElementById("message-input");
+    const messageContainer = document.getElementById("message-container");
+  
+    // Fetch and display existing messages
+    fetch(`${API_URL}/messages`)
+      .then((response) => response.json())
+      .then((messages) => {
+        messages.forEach((message) => {
+          displayMessage(message);
+        });
+      });
+  
+    // Handle form submission
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+  
+      const content = messageInput.value.trim();
+      if (content) {
+        fetch(`${API_URL}/messages`, {
+          method: "POST",
+          headers: {
             "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ content: messageText }),
+          },
+          body: JSON.stringify({ content }),
+        })
+          .then((response) => response.json())
+          .then((message) => {
+            displayMessage(message);
+            messageInput.value = "";
+          });
+      }
     });
-
-    addMessageToChatHistory(messageText);
-    messageInput.value = "";
-});
-
-
+  
+    // Display messages
+    function displayMessage(message) {
+      const messageElement = document.createElement("div");
+      messageElement.textContent = message.content;
+      messageContainer.appendChild(messageElement);
+      messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
+  });
+  
