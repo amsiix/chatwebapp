50a7b6563a5adcb65c6557a12da964b97240cb90
Abel Mendez <amsiix@gmail.com>
2023-04-27 17:11:38 +0000

    Modified to change style

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 // Serve static files
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -47,7 +48,11 @@ app.post("/api/messages", (req, res) => {
     });
 });
 
-// Start the server
-app.listen(port, () => {
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+  // Start the server
+  app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+  });
